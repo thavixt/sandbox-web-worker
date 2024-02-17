6b161b62d99ab8c7c6c3e864198799621db7ecc8
Péter Komlósi <komlosi.peti@google.com>
2024-02-17 16:54:56 +0100

    Updates

--- userCode.js
+++ userCode.js
@@ -1,2 +1,1 @@
-$.mySandboxFunction('DEMO - get increasing number digits of PI');
-// setTimeout(() => $.mySandboxFunction('I was delayed in user code by 10 sec - BUT might not be seen since the sandbox might have been terminated by then (bug?)'), 10_000);
+$.mySandboxFunction('called inline by user');
